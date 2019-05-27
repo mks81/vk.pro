8fb1e361edbb239c005797809015a0c80232d52f
mks <mks.mikhaylov@gmail.com>
2019-05-27 16:37:19 +0200

    UIcontrol

--- vk.pro/Likes.swift
+++ vk.pro/Likes.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class Likes: UIControl {
     
+    var like = Int.random(in: 0...100)
+    var liked = Bool.random()
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         return false
     }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initLikes()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initLikes()
+    }
+    
+    private func initLikes() {
+        //backgroundColor = .blue
+        //label.text = String(like)
+        //image.image = liked ? UIImage(named: "like-filled") : UIImage(named: "like")
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
