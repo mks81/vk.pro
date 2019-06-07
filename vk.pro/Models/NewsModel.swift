795061bb67cd7dd6de021b11a0dd3c3ea2b7d288
mks <mks.mikhailov@gmail.com>
2019-06-07 10:08:23 +0200

    Homework-6 NewsModel

---
+++ vk.pro/Models/NewsModel.swift
@@ -0,0 +1,16 @@
+//
+//  NewsModel.swift
+//  vk.pro
+//
+//  Created by Konstantin Mikhailov on 07/06/2019.
+//  Copyright © 2019 Nikota. All rights reserved.
+//
+
+import Foundation
+
+struct NewsModel {
+    var name: String
+    var avatar: String
+    var text: String
+    var photo: String
+}
