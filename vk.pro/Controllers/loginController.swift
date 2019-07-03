4b5db0efcec8be75668aad02f365af281fa8fd3c
mks <mks.mikhaylov@gmail.com>
2019-07-03 14:08:33 +0200

    Homework-2.2 VK Auth

---
+++ vk.pro/Controllers/loginController.swift
@@ -0,0 +1,82 @@
+//
+//  loginController.swift
+//  vk.pro
+//
+//  Created by mks on 03/07/2019.
+//  Copyright © 2019 Nikota. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import Alamofire
+
+class loginController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView! {
+        didSet {
+            webView.navigationDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        logoutVK()
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "7042669"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.68")
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        
+        webView.load(request)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        guard let token = params["access_token"] else { return }
+        
+        Session.instance.token = token
+        AF.request("https://api.vk.com/method/groups.get?extended=1&access_token=\(Session.instance.token)&v=5.95").responseJSON { (response) in
+            print(response.value)
+        }
+        
+        decisionHandler(.cancel)
+    }
+    
+    func logoutVK() {
+        let dataStore = WKWebsiteDataStore.default()
+        dataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            dataStore.removeData(
+                ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(),
+                for: records.filter { $0.displayName.contains("vk")},
+                completionHandler: { }
+            )
+        }
+    }
+}
